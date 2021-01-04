98837443bb7eddf591c80f8f3633ef33f3b2a175
lucas ayala <lucas1569622193@gmail.com>
2021-01-04 16:02:25 -0300

    espar

---
+++ app.js
@@ -0,0 +1,3 @@
+function esPar(num){
+    num == 0? console.log("Error, el número ingresado no puede ser 0") : num%2 == 0 ? console.log("El número " + num + " es par"): console.log("El número " + num + " es impar")
+}
